f3704223d802901d9f547b69461183480ff23ce5
Jamaal Long <acousticamericana@gmail.com>
2021-01-07 22:55:57 -0600

    * Echo cluster name.

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,5 @@ docker build -t "$AWS_IMAGE_TAG" .
 docker tag "$AWS_IMAGE_TAG":latest  "$AWS_IMAGE_REPO_URL"/"$AWS_IMAGE_TAG":latest
 docker push "$AWS_IMAGE_REPO_URL"/"$AWS_IMAGE_TAG":latest
 
+echo "$AWS_CLUSTER_NAME"
 ecs-deploy -c "$AWS_CLUSTER_NAME" -n "$AWS_SERVICE_NAME" -i "$AWS_IMAGE_REPO_URL":latest
