6d7c680c7cf94cb0842ac11819589e22ebddf6f3
Jamaal Long <acousticamericana@gmail.com>
2021-01-07 21:41:17 -0600

    * Fixed bash errors.

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ docker build -t "$AWS_IMAGE_TAG" .
 docker tag "$AWS_IMAGE_TAG":latest  "$AWS_IMAGE_REPO_URL"/"$AWS_IMAGE_TAG":latest
 docker push "$AWS_IMAGE_REPO_URL"/"$AWS_IMAGE_TAG":latest
 
-ecs-deploy -c "$AWS_CLUSTER_NAME" -n "$AWS_SERVICE_NAME" -i "$AWS_IMAGE_REPO_URL"/"   ":latest
+ecs-deploy -c "$AWS_CLUSTER_NAME" -n "$AWS_SERVICE_NAME" -i "$AWS_IMAGE_REPO_URL":latest
