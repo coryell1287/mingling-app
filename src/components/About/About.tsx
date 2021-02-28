54c56a781e4665fe3f6d5a038b801afed4d6e19a
Jamaal Long <acousticamericana@gmail.com>
2021-02-28 15:36:15 -0600

    Fixing About us content.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,13 +7,12 @@ import { Section } from '@components/Layout/Section';
 export const About = (): React.ReactElement => {
   return (
     <Section>
-      <Row>
-        <Col md={{ span: 6, offset: 3 }}>
+      <Row className="justify-content-md-center">
+        <Col xs lg="5">
           <h3>About Us</h3>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum laboriosam saepe ea architecto assumenda
-            laudantium, facilis autem suscipit, porro aliquid incidunt ad excepturi odit hic veniam minus voluptatem vitae
-            dolores?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum architecto quos exercitationem, sed mollitia, ipsum
+            sit eaque inventore, odio natus omnis assumenda excepturi aperiam! Hic repudiandae quia ad quos eveniet.
           </p>
         </Col>
       </Row>
@@ -21,7 +20,9 @@ export const About = (): React.ReactElement => {
         <Col xs lg="2">
           1 of 3
         </Col>
-        <Col md="auto">Variable width content</Col>
+        <Col xs lg="2">
+          Variable width content
+        </Col>
         <Col xs lg="2">
           3 of 3
         </Col>
--- src/components/Layout/Section/Section.tsx
+++ src/components/Layout/Section/Section.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 
 import classes from '@components/Layout/Section/section.css';
 
@@ -9,9 +8,5 @@ type ContainerProps = {
 };
 
 export const Section = ({ children }: ContainerProps): React.ReactElement => {
-  return (
-    <Container bsPrefix={classes.container}>
-      <Row>{children}</Row>
-    </Container>
-  );
+  return <Container bsPrefix={classes.container}>{children}</Container>;
 };
